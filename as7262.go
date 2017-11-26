a935ba4181a1f81f6541a5f35b2c2d03563358dc
Josh <josh@zool.com>
2017-11-26 12:30:44 +1100

    add virtualregister read add virtualregister write

--- as7262.go
+++ as7262.go
@@ -1,15 +1,18 @@
 package as7262
 
 import (
+	"encoding/binary"
 	"fmt"
 	"log"
 	"time"
 
-	"github.com/NeuralSpaz/i2cmux"
+	"golang.org/x/exp/io/i2c"
+	// "github.com/NeuralSpaz/i2cmux"
 )
 
 type AS7276 struct {
-	dev *i2cmux.Device
+	// dev *i2cmux.Device
+	dev *i2c.Device
 }
 
 // Spectrum (450nm, 500nm, 550nm, 570nm, 600nm, 650nm).
@@ -17,138 +20,240 @@ type Spectrum struct {
 	V, B, G, Y, O, R uint16
 }
 
-func NewSensor(mux i2cmux.Multiplexer, port uint8, opts ...func(*AS7276) error) (*AS7276, error) {
+func NewSensor(bus string, opts ...func(*AS7276) error) (*AS7276, error) {
 	a := new(AS7276)
 
 	for _, option := range opts {
 		option(a)
 	}
 	var err error
-	a.dev, err = i2cmux.Open(0x49, mux, port)
+	// a.dev, err := i2c.Open(bus, 0x49)
+	a.dev, err = i2c.Open(&i2c.Devfs{Dev: bus}, 0x49)
+	// a.dev, err = i2cmux.Open(0x49, mux, port)
 	if err != nil {
 		log.Panic(err)
 	}
+	// a.dev = *dev
 	a.setConfig()
 	return a, nil
 }
+
+func (a *AS7276) virtualRegisterWrite(register, data byte) error {
+	const (
+		SlaveStatusRegister byte = 0x00
+		SlaveWriteRegister  byte = 0x01
+	)
+	for {
+		rx := make([]byte, 1)
+		if err := a.dev.ReadReg(SlaveStatusRegister, rx); err != nil {
+			log.Fatalln(err)
+		}
+		if rx[0] == 0x00 {
+			break
+		}
+		time.Sleep(time.Millisecond)
+	}
+	if err := a.dev.WriteReg(SlaveWriteRegister, []byte{register | 0x80}); err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		rx := make([]byte, 1)
+		if err := a.dev.ReadReg(SlaveStatusRegister, rx); err != nil {
+			log.Fatalln(err)
+		}
+		if rx[0] == 0x00 {
+			break
+		}
+		time.Sleep(time.Millisecond)
+	}
+
+	if err := a.dev.WriteReg(SlaveWriteRegister, []byte{data}); err != nil {
+		log.Fatalln(err)
+	}
+
+	return nil
+}
+
+func (a *AS7276) virtualRegisterRead(register byte) (byte, error) {
+	const (
+		SlaveStatusRegister byte = 0x00
+		SlaveWriteRegister  byte = 0x01
+		SlaveReadRegister   byte = 0x02
+	)
+	for {
+		rx := make([]byte, 1)
+		if err := a.dev.ReadReg(SlaveStatusRegister, rx); err != nil {
+			log.Fatalln(err)
+		}
+		if rx[0] == 0x00 {
+			break
+		}
+		// if there is data pending read it but thats all
+		if rx[0] == 0x01 {
+			rx := make([]byte, 1)
+			if err := a.dev.ReadReg(SlaveStatusRegister, rx); err != nil {
+				log.Fatalln(err)
+			}
+		}
+		time.Sleep(time.Millisecond)
+	}
+
+	if err := a.dev.WriteReg(SlaveWriteRegister, []byte{register}); err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		rx := make([]byte, 1)
+		if err := a.dev.ReadReg(SlaveStatusRegister, rx); err != nil {
+			log.Fatalln(err)
+		}
+		if rx[0] == 0x01 {
+			break
+		}
+		time.Sleep(time.Millisecond)
+	}
+	data := make([]byte, 1)
+	if err := a.dev.ReadReg(SlaveReadRegister, data); err != nil {
+		log.Fatalln(err)
+	}
+
+	return data[0], nil
+}
+
+// func NewSensor(mux i2cmux.Multiplexer, port uint8, opts ...func(*AS7276) error) (*AS7276, error) {
+// 	a := new(AS7276)
+
+// 	for _, option := range opts {
+// 		option(a)
+// 	}
+// 	var err error
+// 	a.dev, err = i2cmux.Open(0x49, mux, port)
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+// 	a.setConfig()
+// 	return a, nil
+// }
+
 func (a *AS7276) Close() error {
 	return a.dev.Close()
 }
 
-const (
-	I2C_AS72XX_SLAVE_STATUS_REG = 0x00
-	I2C_AS72XX_SLAVE_WRITE_REG  = 0x01
-	I2C_AS72XX_SLAVE_READ_REG   = 0x02
-	I2C_AS72XX_SLAVE_TX_VALID   = 0x02
-	I2C_AS72XX_SLAVE_RX_VALID   = 0x01
-)
+// const (
+// 	I2C_AS72XX_SLAVE_STATUS_REG = 0x00
+// 	I2C_AS72XX_SLAVE_WRITE_REG  = 0x01
+// 	I2C_AS72XX_SLAVE_READ_REG   = 0x02
+// 	I2C_AS72XX_SLAVE_TX_VALID   = 0x02
+// 	I2C_AS72XX_SLAVE_RX_VALID   = 0x01
+// )
 
-func (a *AS7276) writeReg(reg byte, buf []byte) error {
-	log.Println("write reg")
-	if err := a.checkTxPending(); err != nil {
-		return err
-	}
+// func (a *AS7276) writeReg(reg byte, buf []byte) error {
+// 	log.Println("write reg")
+// 	if err := a.checkTxPending(); err != nil {
+// 		return err
+// 	}
 
-	// rx := make([]byte, 1)
-	if err := a.dev.WriteReg(I2C_AS72XX_SLAVE_WRITE_REG, []byte{reg | 0x80}); err != nil {
-		return err
-	}
+// 	// rx := make([]byte, 1)
+// 	if err := a.dev.WriteReg(I2C_AS72XX_SLAVE_WRITE_REG, []byte{reg | 0x80}); err != nil {
+// 		return err
+// 	}
 
-	if err := a.checkTxPending(); err != nil {
-		return err
-	}
+// 	if err := a.checkTxPending(); err != nil {
+// 		return err
+// 	}
 
-	err := a.dev.WriteReg(I2C_AS72XX_SLAVE_WRITE_REG, buf)
-	return err
+// 	err := a.dev.WriteReg(I2C_AS72XX_SLAVE_WRITE_REG, buf)
+// 	return err
 
-}
+// }
 
-func (a *AS7276) readReg(reg byte) (byte, error) {
-	log.Println("read reg")
-	if err := a.checkTxPending(); err != nil {
-		return 0, err
-	}
+// func (a *AS7276) readReg(reg byte) (byte, error) {
+// 	log.Println("read reg")
+// 	if err := a.checkTxPending(); err != nil {
+// 		return 0, err
+// 	}
 
-	// rx := make([]byte, 1)
-	if err := a.dev.WriteReg(I2C_AS72XX_SLAVE_WRITE_REG, []byte{reg | 0x80}); err != nil {
-		return 0, err
-	}
+// 	// rx := make([]byte, 1)
+// 	if err := a.dev.WriteReg(I2C_AS72XX_SLAVE_WRITE_REG, []byte{reg | 0x80}); err != nil {
+// 		return 0, err
+// 	}
 
-	if err := a.checkRxPending(); err != nil {
-		return 0, err
-	}
-	buf := make([]byte, 1)
-	err := a.dev.ReadReg(I2C_AS72XX_SLAVE_READ_REG, buf)
-	return buf[0], err
+// 	if err := a.checkRxPending(); err != nil {
+// 		return 0, err
+// 	}
+// 	buf := make([]byte, 1)
+// 	err := a.dev.ReadReg(I2C_AS72XX_SLAVE_READ_REG, buf)
+// 	return buf[0], err
 
-}
+// }
 
-func (a *AS7276) checkRxPending() error {
-	for {
-		log.Println("checking rx pending")
-		rx := make([]byte, 1)
-		if err := a.dev.ReadReg(I2C_AS72XX_SLAVE_STATUS_REG, rx); err != nil {
-			return err
-		}
-		log.Printf("%02x rx\n", rx)
-		if rx[0]&I2C_AS72XX_SLAVE_RX_VALID == 0 {
-			log.Println("checking rx ok")
-			return nil
-		}
-		time.Sleep(time.Millisecond * 10)
-	}
-}
+// func (a *AS7276) checkRxPending() error {
+// 	for {
+// 		log.Println("checking rx pending")
+// 		rx := make([]byte, 1)
+// 		if err := a.dev.ReadReg(I2C_AS72XX_SLAVE_STATUS_REG, rx); err != nil {
+// 			return err
+// 		}
+// 		log.Printf("%02x rx\n", rx)
+// 		if rx[0]&I2C_AS72XX_SLAVE_RX_VALID == 0 {
+// 			log.Println("checking rx ok")
+// 			return nil
+// 		}
+// 		time.Sleep(time.Millisecond * 10)
+// 	}
+// }
 
-func (a *AS7276) checkTxPending() error {
-	for {
-		log.Println("checking tx pending")
-		rx := make([]byte, 1)
-		if err := a.dev.ReadReg(I2C_AS72XX_SLAVE_STATUS_REG, rx); err != nil {
-			log.Printf("rx error %02x %v\n", rx, err)
-			// return err
-		}
-		if rx[0]&I2C_AS72XX_SLAVE_TX_VALID == 0 {
-			log.Printf("checking tx ok, %02x\n", rx)
-			return nil
-		}
-		time.Sleep(time.Millisecond * 10)
-	}
-}
+// func (a *AS7276) checkTxPending() error {
+// 	for {
+// 		log.Println("checking tx pending")
+// 		rx := make([]byte, 1)
+// 		if err := a.dev.ReadReg(I2C_AS72XX_SLAVE_STATUS_REG, rx); err != nil {
+// 			log.Printf("rx error %02x %v\n", rx, err)
+// 			// return err
+// 		}
+// 		if rx[0]&I2C_AS72XX_SLAVE_TX_VALID == 0 {
+// 			log.Printf("checking tx ok, %02x\n", rx)
+// 			return nil
+// 		}
+// 		time.Sleep(time.Millisecond * 10)
+// 	}
+// }
 
 func (a *AS7276) setConfig() error {
 	fmt.Println("setConfig")
 
-	if err := a.writeReg(0x04, []byte{0xE0}); err != nil {
+	if err := a.virtualRegisterWrite(0x04, 0xE0); err != nil {
 		return err
 	}
 	time.Sleep(time.Millisecond * 500)
-	if err := a.writeReg(0x04, []byte{0x60}); err != nil {
+	if err := a.virtualRegisterWrite(0x04, 0x60); err != nil {
 		return err
 	}
-	if err := a.writeReg(0x06, []byte{0xFF}); err != nil {
+	if err := a.virtualRegisterWrite(0x06, 0xFF); err != nil {
 		return err
 	}
 	// LED OFF
-	if err := a.writeReg(0x07, []byte{0x00}); err != nil {
+	if err := a.virtualRegisterWrite(0x07, 0x00); err != nil {
 		return err
 	}
 	return nil
 
 }
 
-func (a *AS7276) clearData() error {
-	fmt.Println("clearData")
-	value, err := a.readReg(0x04)
-	if err != nil {
-		return err
-	}
-	value = setBit(value, 1)
-	if err := a.writeReg(0x04, []byte{value}); err != nil {
-		return err
-	}
+// func (a *AS7276) clearData() error {
+// 	fmt.Println("clearData")
+// 	value, err := a.virtualRegisterRead(0x04)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	value = setBit(value, 1)
+// 	if err := a.writeReg(0x04, []byte{value}); err != nil {
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
 func (a *AS7276) setMode(mode uint8) error {
 	fmt.Println("setmode")
@@ -157,13 +262,13 @@ func (a *AS7276) setMode(mode uint8) error {
 		mode = 3
 	}
 
-	control, err := a.readReg(0x04)
+	control, err := a.virtualRegisterRead(0x04)
 	if err != nil {
 		return err
 	}
 	control &= 0xf3
 	control |= (mode << 2)
-	if err := a.writeReg(0x04, []byte{control}); err != nil {
+	if err := a.virtualRegisterWrite(0x04, control); err != nil {
 		return err
 	}
 	return nil
@@ -173,7 +278,7 @@ func (a *AS7276) dataReady() (bool, error) {
 	fmt.Println("dataReady?")
 	var control byte
 	err := retry(10, time.Millisecond*50, func() (err error) {
-		control, err = a.readReg(0x04)
+		control, err = a.virtualRegisterRead(0x04)
 		return
 	})
 
@@ -210,17 +315,16 @@ func (a *AS7276) ReadAll() (Spectrum, error) {
 	fmt.Println("readall")
 
 	// err := retry(10, time.Millisecond*50, func() (err error) {
-	err := a.clearData()
+	// err := a.clearData()
 
-	if err != nil {
-		log.Println(err)
-	}
+	// if err != nil {
+	// 	log.Println(err)
+	// }
 
-	if err != nil {
-		log.Println(err)
-	}
-	err = a.setMode(3)
-	if err != nil {
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+	if err := a.setMode(3); err != nil {
 		log.Println(err)
 	}
 	ready, err := a.dataReady()
@@ -236,74 +340,73 @@ func (a *AS7276) ReadAll() (Spectrum, error) {
 	}
 
 	// LED ON
-	// if err := a.writeReg(0x07, []byte{0x09}); err != nil {
+	if err := a.virtualRegisterWrite(0x07, 0x09); err != nil {
+		return Spectrum{}, err
+	}
+
+	vh, err := a.virtualRegisterRead(0x08)
+	if err != nil {
+		return Spectrum{}, err
+	}
+	vl, err := a.virtualRegisterRead(0x09)
+	if err != nil {
+		return Spectrum{}, err
+	}
+	bh, err := a.virtualRegisterRead(0x0a)
+	if err != nil {
+		return Spectrum{}, err
+	}
+	bl, err := a.virtualRegisterRead(0x0b)
+	if err != nil {
+		return Spectrum{}, err
+	}
+	gh, err := a.virtualRegisterRead(0x0c)
+	if err != nil {
+		return Spectrum{}, err
+	}
+	gl, err := a.virtualRegisterRead(0x0d)
+	if err != nil {
+		return Spectrum{}, err
+	}
+	yh, err := a.virtualRegisterRead(0x0e)
+	if err != nil {
+		return Spectrum{}, err
+	}
+	yl, err := a.virtualRegisterRead(0x0f)
+	if err != nil {
+		return Spectrum{}, err
+	}
+	oh, err := a.virtualRegisterRead(0x10)
+	if err != nil {
+		return Spectrum{}, err
+	}
+	ol, err := a.virtualRegisterRead(0x11)
+	if err != nil {
+		return Spectrum{}, err
+	}
+	rh, err := a.virtualRegisterRead(0x12)
+	if err != nil {
+		return Spectrum{}, err
+	}
+	rl, err := a.virtualRegisterRead(0x13)
+	if err != nil {
+		return Spectrum{}, err
+	}
+
+	// LED OFF
+	// if err := a.writeReg(0x07, []byte{0x00}); err != nil {
 	// 	return Spectrum{}, err
 	// }
 
-	// vh, err := a.readReg(0x08)
-	// if err != nil {
-	// 	return Spectrum{}, err
-	// }
-	// vl, err := a.readReg(0x09)
-	// if err != nil {
-	// 	return Spectrum{}, err
-	// }
+	v := binary.LittleEndian.Uint16([]byte{vh, vl})
+	b := binary.LittleEndian.Uint16([]byte{bh, bl})
+	g := binary.LittleEndian.Uint16([]byte{gh, gl})
+	y := binary.LittleEndian.Uint16([]byte{yh, yl})
+	o := binary.LittleEndian.Uint16([]byte{oh, ol})
+	r := binary.LittleEndian.Uint16([]byte{rh, rl})
 
-	// bh, err := a.readReg(0x0a)
-	// if err != nil {
-	// 	return Spectrum{}, err
-	// }
-	// bl, err := a.readReg(0x0b)
-	// if err != nil {
-	// 	return Spectrum{}, err
-	// }
-	// gh, err := a.readReg(0x0c)
-	// if err != nil {
-	// 	return Spectrum{}, err
-	// }
-	// gl, err := a.readReg(0x0d)
-	// if err != nil {
-	// 	return Spectrum{}, err
-	// }
-	// yh, err := a.readReg(0x0e)
-	// if err != nil {
-	// 	return Spectrum{}, err
-	// }
-	// yl, err := a.readReg(0x0f)
-	// if err != nil {
-	// 	return Spectrum{}, err
-	// }
-	// oh, err := a.readReg(0x10)
-	// if err != nil {
-	// 	return Spectrum{}, err
-	// }
-	// ol, err := a.readReg(0x11)
-	// if err != nil {
-	// 	return Spectrum{}, err
-	// }
-	// rh, err := a.readReg(0x12)
-	// if err != nil {
-	// 	return Spectrum{}, err
-	// }
-	// rl, err := a.readReg(0x13)
-	// if err != nil {
-	// 	return Spectrum{}, err
-	// }
-
-	// // LED OFF
-	// // if err := a.writeReg(0x07, []byte{0x00}); err != nil {
-	// // 	return Spectrum{}, err
-	// // }
-
-	// v := binary.LittleEndian.Uint16([]byte{vh, vl})
-	// b := binary.LittleEndian.Uint16([]byte{bh, bl})
-	// g := binary.LittleEndian.Uint16([]byte{gh, gl})
-	// y := binary.LittleEndian.Uint16([]byte{yh, yl})
-	// o := binary.LittleEndian.Uint16([]byte{oh, ol})
-	// r := binary.LittleEndian.Uint16([]byte{rh, rl})
-
-	// return Spectrum{v, b, g, y, o, r}, nil
-	return Spectrum{}, nil
+	return Spectrum{v, b, g, y, o, r}, nil
+	// return Spectrum{}, nil
 }
 
 func clearBit(n byte, pos uint8) byte {
--- example/main.go
+++ example/main.go
@@ -6,20 +6,22 @@ import (
 	"time"
 
 	"github.com/NeuralSpaz/as7262"
-
-	"github.com/NeuralSpaz/pca9548a"
 )
 
 func main() {
 	fmt.Println("starting sensor")
-	mux, err := pca9548a.NewMux("/dev/i2c-1")
-	defer mux.Close()
+	// mux, err := pca9548a.NewMux("/dev/i2c-1")
+	// defer mux.Close()
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	// sensor, err := as7262.NewSensor(mux, 0)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	sensor, err := as7262.NewSensor("/dev/i2c-1")
 	if err != nil {
-		log.Panic(err)
-	}
-	sensor, err := as7262.NewSensor(mux, 0)
-	if err != nil {
-		log.Panic(err)
+		log.Fatalln(err)
 	}
 	defer sensor.Close()
 	log.Println(sensor)
